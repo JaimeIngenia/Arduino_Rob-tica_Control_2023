fbbfa6aecf487ed6e11c8b2bcff45d89ba4f5743
jaime1315321 <jamoncayop@unal.edu.co>
2023-03-05 11:46:40 -0500

    intento de Timmer1

---
+++ 20_Base_timmerOne_Jaime_Sabado_Marzo/20_Base_timmerOne_Jaime_Sabado_Marzo.ino
@@ -0,0 +1,39 @@
+float  pv; //Variable de proceso coentiene la salida de PWM
+int pinA = 3;
+volatile long contador = 0;
+unsigned long previousMillis = 0;
+long interval = 1000;
+unsigned long time_start;
+
+void setup() {
+  pinMode(pinA, INPUT);
+  Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(pinA), interrupcion,RISING);
+  cli();                   //Desabilito las interrupciones
+  TCCR1A = 0;              //El registro de control A queda todo en 0
+  TCCR1B = 0;              //Limpia registrador
+  TCCR1B |= B00000101;   //MODO CTC, prescaler //TCCR1B |= B00000100; //TCCR1B |= ( 1 << cs10 ) | ( 1 << cs12 ); 
+  
+  TCNT1   = 0;              //Inicializa el temporizador
+
+  OCR1A  =  156.25;        //Carga el registrador de comparación: [ 16MHz / (Pre-escalador * FrecuenciaInterrupcionDeseada)] - 1   o dividir los pulsos del cristal entre la frecuencia deseada
+  
+  TIMSK1 |= ( 1 << OCIE1A); //Habilita interrupción por igualdad de comparacion // TIMSK1 |= B00000010;
+  sei(); //Habilito las interrupciones
+}
+
+void loop() {
+  Serial.println(pv);  
+}
+
+ISR(TIMMER1_COMPA_vect){    //Interrupción por igualdad de compación
+  
+  pv = contador*(60.0/27000.0); //RPM en eje de salida  
+  Serial.println(pv);  
+  contador = 0;
+  TCNT1 = 0;      //Reestablecer el valor del temporizador porque si no contará hasta su valor máximo
+}
+
+void interrupcion(){
+  contador++;
+  }
